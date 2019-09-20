2a00696630161a01482e08fe9f9edf5e92561e70
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 23:13:21 +0200

    footer show where we are on page

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import {Route} from "react-router-dom";
+import "../style/Footer.css";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <h2>Footer</h2>
+
+            <Route path='/' exact render={() => {
+                return (
+                    <p>You are on <span>main page</span></p>
+                )
+            }}/>
+
+            <Route path='/:page' exact render={props => {
+                return (
+                    <>
+                    <p>You are on <span>{props.match.params.page}</span></p>
+                    <p>You are on <span>{props.match.url}</span></p>
+                    <p>You are on <span>{props.match.path}</span></p>
+                    </>
+                )
+            }}/>
+
+            <Route path='/:page/:idProduct' exact render={props => {
+                return (
+                    <>
+                    <p>You are on <span>{props.match.params.idProduct}</span></p>
+                    <p>You are on <span>{props.match.url}</span></p>
+                    <p>You are on <span>{props.match.path}</span></p>
+                    </>
+                )
+            }}/>
+        </div>
     )
 };
 
