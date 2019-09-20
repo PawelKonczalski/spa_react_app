282afc5a76a834c601dd5d368af34a219d92f6a9
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 21:11:13 +0200

    contact page and protection against loss of messages from the form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import '../style/ContactPage.css'
+import {Prompt} from "react-router";
 
-const ContactPage = () => {
-    return (
-        <div>ContactPage</div>
-    )
-};
+class ContactPage extends React.Component {
+
+    state = {
+        value: ''
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState({
+            value: ''
+        })
+    };
+
+    handleChange = e => {
+        this.setState({
+            value: e.target.value
+        })
+    };
+
+    render() {
+        return (
+            <div className='contact'>
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Write to us</h3>
+                    <textarea value={this.state.value} onChange={this.handleChange}
+                              placeholder='type a message...'/>
+                    <button>Send massage</button>
+                </form>
+                <Prompt when={this.state.value}
+                        message={"You didn't send the message. Are you sure you want to leave this page?"}/>
+            </div>
+        )
+    }
+}
 
 export default ContactPage
