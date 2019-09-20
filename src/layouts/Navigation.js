7cfdfed322363b5b46068d5133d66fe82a7d7cba
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-20 19:59:18 +0200

    navigation

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import '../style/Navigation.css'
+
+const list = [
+    {name: 'start', path: '/', exact: true},
+    {name: 'products', path: '/products'},
+    {name: 'contact', path: '/contact'},
+    {name: 'admin panel', path: '/admin'},
+];
 
 const Navigation = () => {
+
+    const menu = list.map(item => (
+        <li>
+            <NavLink to={item.path} exact={item.exact ? item.exact : false}>{item.name}</NavLink>
+        </li>
+    ));
+
     return (
-        <div>Nav</div>
+        <nav className='main'>
+            <ul>
+                {menu}
+            </ul>
+        </nav>
     )
 };
 
